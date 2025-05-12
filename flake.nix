d2b450cfa0d60aacdd5fb504667962aaf078e309
mkhoatd <mkhoatd@gmail.com>
2025-05-12 16:02:22 +0700

    save

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "mkhoatd Darwin system flake";
 
-  inputs = {
+  inputs = rec {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
